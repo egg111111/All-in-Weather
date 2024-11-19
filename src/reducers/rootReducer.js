6ea19a0c1108186a292a0de71bc5d02ef4d81772
egg <chaeyun12338@gmail.com>
2024-11-19 09:26:20 +0900

    recView 수정

--- src/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer'; // 사용자 관련 리듀서 예시
-
-export default combineReducers({
-    user: userReducer,
-    // 다른 리듀서도 여기에 추가할 수 있습니다.
-});
